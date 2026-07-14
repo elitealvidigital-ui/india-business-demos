02f76bbcb0e92678810f62699523697d25e8862e
elitealvidigital-ui <elitealvidigital@gmail.com>
2026-07-15 00:16:23 +0530

    Publish India generated demos

--- india-tracking-config.js
+++ india-tracking-config.js
@@ -3,6 +3,6 @@
 window.INDIA_DEMO_TRACKING = Object.freeze({
   version: '1',
   enabled: true,
-  endpoint: 'https://script.google.com/macros/s/AKfycbyoU1toM2TDNjQQQXlRQAHxVxUxiqQzuY3tclAMGJeQ2qU_HGS9JINDMzOXGw0gvt7H/exec',
+  endpoint: 'https://script.google.com/macros/s/AKfycbz7V8xZFi1hsiN7V-VXUN9qRPYym4gdHNYUn8ZyqePJ52OIQ5lYmen0TF8oddlWYf0/exec',
   source: 'india_demo',
 });
